48a72200944779d801f1ce588636e576f338fec1
yuuddin <jindf51@naver.com>
2024-07-14 18:47:21 +0900

    health check api 추가

---
+++ src/main/java/com/umc/owncast/RootController.java
@@ -0,0 +1,13 @@
+package com.umc.owncast;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OwnCast health check";
+    }
+}
