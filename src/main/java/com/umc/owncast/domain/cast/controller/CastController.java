ea3ab1a3b86e65df74605dcd85592fa3e6f30f4e
yuuddin <jindf51@naver.com>
2024-07-28 13:35:08 +0900

    add: @구분자를 통한 parsing 로직 구현

--- src/main/java/com/umc/owncast/domain/cast/controller/CastController.java
+++ src/main/java/com/umc/owncast/domain/cast/controller/CastController.java
@@ -2,10 +2,11 @@ package com.umc.owncast.domain.cast.controller;
 
 import com.umc.owncast.common.response.ApiResponse;
 import com.umc.owncast.domain.cast.dto.CastCreationRequestDTO;
+import com.umc.owncast.domain.cast.dto.TTSDTO;
 import com.umc.owncast.domain.cast.service.CastService;
 import com.umc.owncast.domain.cast.service.ChatGPTScriptService;
 import com.umc.owncast.domain.cast.service.ScriptService;
-import com.umc.owncast.domain.cast.service.StreamService;
+import com.umc.owncast.domain.cast.service.TTSService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -23,7 +24,8 @@ import java.net.MalformedURLException;
 public class CastController {
     private final CastService castService;
     private final ScriptService scriptService;
-    private final StreamService streamService;
+    private final TTSService ttsService;
+    //private final StreamService streamService;
 
     /* ScriptService 테스트 */
     @PostMapping("/script")
@@ -31,6 +33,13 @@ public class CastController {
     public String createScript(@Valid @RequestBody CastCreationRequestDTO castRequest){
         System.out.println(castRequest);
         return scriptService.createScript(castRequest);
+    }
+
+    /* TTS Service 테스트*/
+    @PostMapping("/tts")
+    @Operation(summary = "tts & timepoint 생성 test api")
+    public String ttsTest(@Valid @RequestBody TTSDTO ttsdto) {
+        return ttsService.createSpeech(ttsdto);
     }
 
     @PostMapping
