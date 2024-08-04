8a8fc7d69fc5ae07ce1d5c7e5ab330ecf1700aaa
yuuddin <127373499+yuuddin@users.noreply.github.com>
2024-08-05 00:24:47 +0900

    add: script 번역 기능 구현 (#28)

--- src/main/java/com/umc/owncast/domain/cast/controller/CastController.java
+++ src/main/java/com/umc/owncast/domain/cast/controller/CastController.java
@@ -3,14 +3,11 @@ package com.umc.owncast.domain.cast.controller;
 import com.umc.owncast.domain.cast.dto.CastCreationRequestDTO;
 import com.umc.owncast.domain.cast.service.CastService;
 import com.umc.owncast.domain.cast.service.ScriptService;
-import com.umc.owncast.domain.cast.service.TTSService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Tag(name = "캐스트 API", description = "캐스트 관련 API입니다")
 @RestController
@@ -19,7 +16,6 @@ import java.util.List;
 public class CastController {
     private final CastService castService;
     private final ScriptService scriptService;
-    //private final StreamService streamService;
 
     /* ScriptService 테스트 */
     @PostMapping("/script")
@@ -30,7 +26,7 @@ public class CastController {
     }
 
     /*cast 저장 전 api*/
-    @PostMapping("temporary")
+    @PostMapping("/temporary")
     @Operation(summary = "스크립트 생성 api. 저장 버튼 전 화면 입니다.")
     public String createCast(@Valid @RequestBody CastCreationRequestDTO castRequest){
         return castService.createCast(castRequest);
