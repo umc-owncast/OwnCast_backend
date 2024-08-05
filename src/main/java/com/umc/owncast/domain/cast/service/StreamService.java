20ae35416b96bf19731eaf0192614c6bb431bf09
Jaehwan Hyun <jec0101@naver.com>
2024-08-05 23:26:28 +0900

    add: castId 스트리밍 추가

--- src/main/java/com/umc/owncast/domain/cast/service/StreamService.java
+++ src/main/java/com/umc/owncast/domain/cast/service/StreamService.java
@@ -1,5 +1,9 @@
 package com.umc.owncast.domain.cast.service;
 
+import com.umc.owncast.domain.cast.entity.Cast;
+import com.umc.owncast.domain.cast.repository.CastRepository;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRange;
@@ -13,11 +17,24 @@ import java.nio.file.Paths;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class StreamService {
+    private final CastRepository castRepository;
     private static final String AUDIO_FILES_PATH = "src/main/resources/stream-test/"; // 이후 수정
 
     public Object stream(String filename, HttpHeaders headers) throws IOException {
         Path filePath = Paths.get(AUDIO_FILES_PATH).resolve(filename).normalize();
+        return streamResource(headers, filePath);
+    }
+
+    public Object stream(Long castId, HttpHeaders headers) throws IOException {
+        Cast cast = castRepository.findById(castId).orElseThrow(() -> new IllegalArgumentException("castId가 잘못되었습니다"));
+        Path filePath = Paths.get(AUDIO_FILES_PATH).resolve(cast.getFilePath()).normalize();
+        return streamResource(headers, filePath);
+    }
+
+    @NotNull
+    private ResponseEntity<Object> streamResource(HttpHeaders headers, Path filePath) throws IOException {
         UrlResource resource = new UrlResource(filePath.toUri());
 
         if (!resource.exists()) {
