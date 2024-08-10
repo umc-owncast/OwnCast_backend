6355c88ffa78e5ea892c4783970a6f1ab812df78
Jaehwan Hyun <jec0101@naver.com>
2024-08-10 19:06:48 +0900

    fix: stream 기능 업데이트 - aws에 파일 업로드함에 따른 변경

--- src/main/java/com/umc/owncast/domain/castplaylist/repository/CastPlaylistRepository.java
+++ src/main/java/com/umc/owncast/domain/castplaylist/repository/CastPlaylistRepository.java
@@ -4,6 +4,9 @@ import com.umc.owncast.domain.castplaylist.entity.CastPlaylist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CastPlaylistRepository extends JpaRepository<CastPlaylist, Long> {
+    Optional<CastPlaylist> findByCastIdAndPlaylistId(Long castId, Long id);
 }
