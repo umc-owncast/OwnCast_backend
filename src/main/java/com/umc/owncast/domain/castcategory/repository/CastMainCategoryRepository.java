b9ae20da96769d524270e01b2dea388ce09ddacc
YamYamee <82762402+YamYamee@users.noreply.github.com>
2024-08-23 11:50:12 +0900

    fix: 회원 기능 연동  (#83)
    빼먹었던 부분 연동 (changePlaylist)
    
    ---------
    
    Co-authored-by: Jaehwan Hyun <jec0101@naver.com>
    Co-authored-by: younheejae <hjy10045@sookmyung.ac.kr>

--- src/main/java/com/umc/owncast/domain/castcategory/repository/CastMainCategoryRepository.java
+++ src/main/java/com/umc/owncast/domain/castcategory/repository/CastMainCategoryRepository.java
@@ -4,6 +4,7 @@ import com.umc.owncast.domain.castcategory.entity.CastMainCategory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
