0b65505508d3c68ad41ce87b30542999d4e962d8
yuuddin <hyj032564@gmail.com>
2024-08-28 17:50:21 +0900

    add: home api에 회원 정보 포함

--- src/main/java/com/umc/owncast/domain/cast/service/chatGPT/keyword/KeywordServiceImpl.java
+++ src/main/java/com/umc/owncast/domain/cast/service/chatGPT/keyword/KeywordServiceImpl.java
@@ -5,6 +5,7 @@ import com.google.gson.reflect.TypeToken;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.umc.owncast.common.exception.handler.UserHandler;
 import com.umc.owncast.common.response.status.ErrorCode;
+import com.umc.owncast.domain.cast.dto.HomeDTO;
 import com.umc.owncast.domain.cast.service.chatGPT.ChatGptAnswerGenerator;
 import com.umc.owncast.domain.cast.service.chatGPT.ChatGptPromptGenerator;
 import com.umc.owncast.domain.category.repository.MainCategoryRepository;
@@ -27,7 +28,7 @@ public class KeywordServiceImpl implements KeywordService {
     private final SubCategoryRepository subCategoryRepository;
 
     @Override
-    public List<String> createKeyword(Member member) {
+    public HomeDTO createKeyword(Member member) {
 
         String script = "";
         List<String> keywords = null;
@@ -55,6 +56,10 @@ public class KeywordServiceImpl implements KeywordService {
             System.out.println("Exception cause : " + e.getCause());
             throw e;
         }
-        return keywords;
+        return HomeDTO.builder()
+                .nickName(member.getNickname())
+                .subCategory(subCategoryName)
+                .keywords(keywords)
+                .build();
     }
 }
