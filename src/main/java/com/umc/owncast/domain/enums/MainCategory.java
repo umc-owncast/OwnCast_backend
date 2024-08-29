e9e7ea9216c3f4b31eb776f0b6a4bda135dc6933
Jaehwan Hyun <jec0101@naver.com>
2024-08-30 02:29:24 +0900

    fix: 회원가입 시 입력된 문자열을 enum으로 변환하도록 변경 - MainCategory enum화에 따른 변경

--- src/main/java/com/umc/owncast/domain/enums/MainCategory.java
+++ src/main/java/com/umc/owncast/domain/enums/MainCategory.java
@@ -1,5 +1,6 @@
 package com.umc.owncast.domain.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -19,5 +20,46 @@ public enum MainCategory {
 
     public String getKrSubCategory() {
         return this.subCategory;
+    }
+
+    @JsonCreator
+    /** String에서 MainCategory 추출 */
+    public static MainCategory fromValue(String value) {
+        String normalizedValue = value.toUpperCase().strip();
+
+        switch(normalizedValue){
+            case "스포츠":
+                return SPORTS;
+            case "음악":
+                return MUSIC;
+            case "책":
+                return BOOK;
+            case "미술":
+                return ART;
+            case "드라마":
+            case "영화":
+            case "드라마/영화":
+            case "영화/드라마":
+                return DRAMA;
+            case "음식":
+                return FOOD;
+            case "시사":
+            case "뉴스":
+            case "시사/뉴스":
+            case "뉴스/시사":
+                return NEWS;
+            case "직접 입력":
+                return ETC;
+            default:
+                break;
+        }
+
+        for (MainCategory category : MainCategory.values()) {
+            if (category.name().equals(normalizedValue)) {
+                return category;
+            }
+        }
+
+        throw new IllegalArgumentException("Unknown main category: " + value);
     }
 }
