e42e32b4d49322013e87bfa4824f9e454fcbed9b
JaehwanH <54016683+ja7811@users.noreply.github.com>
2024-08-30 09:34:33 +0900

    fix: 회원가입 시 입력된 문자열을 enum으로 변환하도록 변경 (#102)
    - MainCategory enum화에 따른 변경

--- src/main/java/com/umc/owncast/domain/member/service/MemberService.java
+++ src/main/java/com/umc/owncast/domain/member/service/MemberService.java
@@ -108,7 +108,7 @@ public class MemberService {
     }
 
     public MemberSettingResponseDTO preferSetting(Member member, MemberPreferRequestDTO memberPreferRequestDTO) {
-        MainCategory mainCategory = MainCategory.valueOf(memberPreferRequestDTO.getMainCategory().toUpperCase(Locale.ROOT));
+        MainCategory mainCategory = MainCategory.fromValue(memberPreferRequestDTO.getMainCategory());
         SubCategory subCategory = subCategoryRepository.findByName(memberPreferRequestDTO.getSubCategory())
                 .orElseGet(() -> {
                     if (memberPreferRequestDTO.getSubCategory() == null) {
