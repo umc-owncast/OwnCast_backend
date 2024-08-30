581623431947075ab9d4004823f0bbee60bdf6d8
Jaehwan Hyun <jec0101@naver.com>
2024-08-30 20:43:11 +0900

    update: CommandLineRunner 구현하는 방식으로 변경 - 빈 주입받아서 필요할 때만 main 실행시키는 방식을 원했는데..  이상한 버그 뜨면서 안되네여  일단 필요할 때만 run 안에 코드 주석해제해서 돌리는 방식으로 갑시다

--- src/main/java/com/umc/owncast/common/initializer/VoiceExampleGenerator.java
+++ src/main/java/com/umc/owncast/common/initializer/VoiceExampleGenerator.java
@@ -5,9 +5,11 @@ import com.umc.owncast.domain.cast.dto.CastScriptDTO;
 import com.umc.owncast.domain.cast.dto.ScriptCastCreationDTO;
 import com.umc.owncast.domain.cast.service.CastService;
 import com.umc.owncast.domain.enums.Formality;
+import com.umc.owncast.domain.enums.Language;
 import com.umc.owncast.domain.enums.VoiceCode;
 import com.umc.owncast.domain.member.entity.Member;
 import com.umc.owncast.domain.member.repository.MemberRepository;
+import com.umc.owncast.domain.sentence.service.TranslationService;
 import com.umc.owncast.domain.voiceexample.entity.VoiceExample;
 import com.umc.owncast.domain.voiceexample.repository.VoiceExampleRepository;
 import lombok.RequiredArgsConstructor;
@@ -24,33 +26,38 @@ public class VoiceExampleGenerator implements CommandLineRunner {
     private final VoiceExampleRepository voiceExampleRepository;
     private final MemberRepository memberRepository;
     private final CastService castService;
+    private final TranslationService translationService;
 
     /**
      * 엔트리 포인트 1
-     * 어플리케이션 실행할 때마다 작동..
-     * 굳이 VoiceExample을 매번 체크할 필요는 없고, 앱 실행시마다 작동시키기엔 오래걸릴 것 같지만
-     * 아래 main()이 잘 작동하지 않으므로 일단 이걸로 갑시다
+     * 어플리케이션 실행할 때마다 작동
+     * -> 쓸 거면 아래 주석 해제하고 실행하시면 됩니다
+     * (아래 main()이 잘 작동하지 않으므로 일단 이걸로 갑시다)
      * */
     @Override
     public void run(String... args) throws Exception {
-        ㅊㅁ
+        /* 실행하려면, 아래 주석 해제하고 실행 한 번 돌리시면 됩니다 (모두 생성하는 데 몇 분 걸림) */
+        //initRepository();
+        //generate("Welcome to Owncast!"); // 대사는 원하는 대로 수정 가능
     }
 
     /**
      * 엔트리 포인트 2
      * VoiceCode 바뀌거나, 예시 음성 바꿀 때만 main 실행
-     * -> 근데 잘 작동 안 함..
+     * -> 근데 작동 안 함..
      * */
     public static void main(String[] args) {
         // ApplicationContext에서 빈 주입
         ApplicationContext context = new AnnotationConfigApplicationContext(OwncastApplication.class);
-        VoiceExampleRepository voiceExampleRepository = context.getBean(VoiceExampleRepository.class);
-        MemberRepository memberRepository = context.getBean(MemberRepository.class);
-        CastService castService = context.getBean(CastService.class);
+        VoiceExampleRepository _voiceExampleRepository = context.getBean(VoiceExampleRepository.class);
+        MemberRepository _memberRepository = context.getBean(MemberRepository.class);
+        CastService _castService = context.getBean(CastService.class);
+        TranslationService _translationService = context.getBean(TranslationService.class);
 
-        VoiceExampleGenerator self = new VoiceExampleGenerator(voiceExampleRepository,
-                memberRepository,
-                castService);
+        VoiceExampleGenerator self = new VoiceExampleGenerator(_voiceExampleRepository,
+                _memberRepository,
+                _castService,
+                _translationService);
         self.initRepository();
         self.generate("Welcome to Owncast!");  // 예시 대사 : "Welcome to Owncast!" -> 나중에 바꿔도 됨
     }
@@ -58,11 +65,21 @@ public class VoiceExampleGenerator implements CommandLineRunner {
     
     /** 주어진 대사로 오디오 생성해서 VoiceExample 테이블에 저장 */
     public void generate(final String PHRASE){
+        final String EN_PHRASE = translationService.translateToMemberLanguage(PHRASE, Language.US);
+        final String JA_PHRASE = translationService.translateToMemberLanguage(PHRASE, Language.JA);
+        final String ES_PHRASE = translationService.translateToMemberLanguage(PHRASE, Language.ES);
         System.out.println("VoiceExampleGenerator: Generating Voice Example...");
+        System.out.printf("en:%s / ja:%s / es:%s\n", EN_PHRASE, JA_PHRASE, ES_PHRASE);
         final Member OWNCAST_MEMBER = memberRepository.findByLoginId("owncast").orElseThrow(); // owncast 계정으로 캐스트 생성
 
         for(VoiceCode voiceCode : VoiceCode.values()){
-            generate(PHRASE, voiceCode, OWNCAST_MEMBER);
+            String phrase = switch(voiceCode.name().substring(0, 2).toLowerCase()){
+                case "en" -> EN_PHRASE;
+                case "ja" -> JA_PHRASE;
+                case "es" -> ES_PHRASE;
+                default -> throw new IllegalStateException("Unexpected language from voiceCode: " + voiceCode.name());
+            };
+            generate(phrase, voiceCode, OWNCAST_MEMBER);
         }
     }
 
