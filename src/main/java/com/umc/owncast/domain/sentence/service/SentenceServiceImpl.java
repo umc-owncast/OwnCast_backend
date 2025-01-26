e55374e3935b8b7a3adc8ff3f3fd02735efdce8c
Jaehwan Hyun <jec0101@naver.com>
2025-01-27 03:29:06 +0900

    update: 오버로딩한 saveSentence 구현

--- src/main/java/com/umc/owncast/domain/sentence/service/SentenceServiceImpl.java
+++ src/main/java/com/umc/owncast/domain/sentence/service/SentenceServiceImpl.java
@@ -38,14 +38,24 @@ public class SentenceServiceImpl implements SentenceService {
             Sentence savedSentence = sentenceRepository.save(sentence);
             sentences.add(savedSentence);
         }
-        System.out.println();
         return sentences;
     }
 
     @Override
     public List<Sentence> saveSentences(String[] parsedOriginalScript, String[] parsedKoreanScript, TTSResultDTO ttsResultDTO, Cast cast) {
-        // todo
-        return null;
+        // todo ttsResult.timePointList / originalScript / koreanScript 인덱스 다를 때 처리 (warning 띄우던가.. 셋 중에 최소인 인덱스로 몰아준다던가..)
+        List<Sentence> sentences = new ArrayList<>(Math.min(parsedKoreanScript.length, parsedOriginalScript.length));
+        for (int i=0; i< ttsResultDTO.getTimePointList().size()-1; i++) {
+            Sentence sentence = Sentence.builder()
+                    .cast(cast)
+                    .originalSentence(parsedOriginalScript[i])
+                    .translatedSentence(parsedKoreanScript[i])
+                    .timePoint(ttsResultDTO.getTimePointList().get(i))
+                    .build();
+            Sentence savedSentence = sentenceRepository.save(sentence);
+            sentences.add(savedSentence);
+        }
+        return sentences;
     }
 
     @Override
