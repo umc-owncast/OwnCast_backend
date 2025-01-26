541ec921ba4c8ca66a29df21feaa5da344446ccb
Jaehwan Hyun <jec0101@naver.com>
2025-01-27 03:24:16 +0900

    add: AsyncConfig 추가

---
+++ src/main/java/com/umc/owncast/common/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package com.umc.owncast.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    @Bean("DefaultExecutor")
+    public Executor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(50);
+        executor.setKeepAliveSeconds(60);
+        executor.setThreadNamePrefix("Default-");
+        executor.initialize();
+        return executor;
+    }
+}
