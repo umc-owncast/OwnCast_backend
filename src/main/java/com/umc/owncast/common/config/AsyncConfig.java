4af33ef3439fb475ed35b07e5253412e06cd09ce
Jaehwan Hyun <jec0101@naver.com>
2025-01-30 19:24:45 +0900

    update: ThreadPoolTaskExecutor 설정 변경

--- src/main/java/com/umc/owncast/common/config/AsyncConfig.java
+++ src/main/java/com/umc/owncast/common/config/AsyncConfig.java
@@ -5,16 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
 public class AsyncConfig {
     @Bean("DefaultExecutor")
-    public Executor threadPoolTaskExecutor() {
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(5);
-        executor.setMaxPoolSize(10);
+        executor.setMaxPoolSize(25);
         executor.setQueueCapacity(50);
         executor.setKeepAliveSeconds(60);
         executor.setThreadNamePrefix("Default-");
