ef67874a47268a7600cca0e6cdcab6ac0ea4115f
yuuddin <hyj032564@gmail.com>
2025-02-16 01:35:09 +0900

    feat: prompt 랜덤 생성 기능 구현

---
+++ src/main/java/com/umc/owncast/common/config/PromptConfig.java
@@ -0,0 +1,20 @@
+package com.umc.owncast.common.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@ConfigurationProperties(prefix = "prompt")
+public class PromptConfig {
+    private List<String> list;
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+}
