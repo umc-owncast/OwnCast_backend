8d441e383f29cb621917f335deb78545a317bf2c
YamYamee <82762402+YamYamee@users.noreply.github.com>
2025-02-18 15:29:27 +0900

    add: 레디스 기본 구성 (#137)

---
+++ src/main/java/com/umc/owncast/common/config/RedisCacheConfig.java
@@ -0,0 +1,46 @@
+package com.umc.owncast.common.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@EnableCaching
+@Configuration
+public class RedisCacheConfig {
+
+    // 기본 캐시 매니저
+    // 다른 캐시 매니저 만들거면 Qualifer 어노테이션 써야 함
+    @Bean
+    @Primary
+    public CacheManager GeneralCacheManager(RedisConnectionFactory redisConnectionFactory) {
+
+        // 기본으로 5시간으로 설정
+        RedisCacheConfiguration redisCacheConfiguration = generateCacheConfiguration()
+                .entryTtl(Duration.ofHours(5L));
+
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+
+    private RedisCacheConfiguration generateCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new StringRedisSerializer()))
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJackson2JsonRedisSerializer()));
+    }
+}
